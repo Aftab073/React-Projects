3a028c3737e3c076f276d2acf349053c7dc2b4c7
Aftab073 <tamboliaftab84@gmail.com>
2024-08-14 20:57:20 +0530

    mini Project on Context

---
+++ miniContext/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import UserContextProvider from './Context/userContextProvider'
+
+function App() {
+
+  return (
+    <UserContextProvider>
+      <h1>Bismillah</h1>
+    </UserContextProvider>
+  )
+}
+
+export default App
